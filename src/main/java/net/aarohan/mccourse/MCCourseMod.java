bb4cf4c2c4caa8e9ef3a0d27614ab400353d5fec
am1234567890 <shar.rohini@gmail.com>
2023-07-27 10:04:01 -0700

    Initial commit

---
+++ src/main/java/net/aarohan/mccourse/MCCourseMod.java
@@ -0,0 +1,80 @@
+package net.aarohan.mccourse;
+
+import com.mojang.logging.LogUtils;
+import net.aarohan.mccourse.block.ModBlocks;
+import net.aarohan.mccourse.enchantment.ModEnchantments;
+import net.aarohan.mccourse.item.ModItems;
+import net.aarohan.mccourse.painting.ModPaintings;
+import net.aarohan.mccourse.sound.ModSounds;
+import net.aarohan.mccourse.util.ModItemProperties;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraft.world.level.block.FlowerPotBlock;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MCCourseMod.MOD_ID)
+public class MCCourseMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "mccourse";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+
+    public MCCourseMod()
+    {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
+        ModEnchantments.register(eventBus);
+        ModSounds.register(eventBus);
+        ModPaintings.register(eventBus);
+
+        // Register the commonSetup method for modloading
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.CHERRY_BLOSSOM_DOOR.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.CHERRY_BLOSSOM_TRAPDOOR.get(), RenderType.cutout());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.TURNIP_CROP.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.PINK_ROSE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.POTTED_PINK_ROSE.get(), RenderType.cutout());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.COBALT_BLASTER.get(), RenderType.cutout());
+
+
+
+
+        ModItemProperties.addCustomItemProperties();
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            ComposterBlock.COMPOSTABLES.put(ModItems.TURNIP_SEEDS.get(), 0.3f);
+            ComposterBlock.COMPOSTABLES.put(ModItems.TURNIP .get(), 0.65f);
+
+            ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(ModBlocks.PINK_ROSE.getId(), ModBlocks.POTTED_PINK_ROSE);
+
+        });
+
+    }
+
+}
---
+++ src/main/java/net/aarohan/mccourse/item/custom/DowsingRodItem.java
@@ -0,0 +1,98 @@
+package net.aarohan.mccourse.item.custom;
+
+import net.aarohan.mccourse.item.ModItems;
+import net.aarohan.mccourse.sound.ModSounds;
+import net.aarohan.mccourse.util.InventoryUtil;
+import net.aarohan.mccourse.util.ModTags;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+import java.util.List;
+
+public class   DowsingRodItem extends Item {
+    public DowsingRodItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        if(pContext.getLevel().isClientSide()) {
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+            boolean foundBlock = false;
+
+            for(int i = 0; i <= positionClicked.getY() + 64; i++) {
+                BlockState blockBelow = pContext.getLevel().getBlockState(positionClicked.below(i));
+
+                if(isValuableBlock(blockBelow)) {
+                    outputValuableCoordinates(positionClicked.below(i), player, blockBelow.getBlock());
+                    foundBlock = true;
+
+                    if(InventoryUtil.hasPlayerStackInInventory(player, ModItems.DATA_TABLET.get())) {
+                        addNbtToDataTablet(player, positionClicked.below(i), blockBelow.getBlock());
+                    }
+
+                    pContext.getLevel().playSound(player, positionClicked, ModSounds.DOWSING_ROD_FOUND_ORE.get(), SoundSource.BLOCKS, 1f, 1f);
+
+
+                    break;
+                }
+            }
+
+            if(!foundBlock) {
+                player.sendSystemMessage(Component.translatable("item.mccourse.dowsing_rod.no_valuables"));
+            }
+        }
+
+        pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(),
+                (player) -> player.broadcastBreakEvent(player.getUsedItemHand()));
+
+        return super.useOn(pContext);
+    }
+    private void addNbtToDataTablet(Player player, BlockPos pos, Block blockBelow) {
+        ItemStack dataTablet =
+                player.getInventory().getItem(InventoryUtil.getFirstInventoryIndex(player, ModItems.DATA_TABLET.get()));
+
+        CompoundTag nbtData = new CompoundTag();
+        nbtData.putString("mccourse.last_ore", "Found " + blockBelow.getName() + " at (" +
+                pos.getX() + ", "+ pos.getY() + ", "+ pos.getZ() + ")");
+
+        dataTablet.setTag(nbtData);
+    }
+
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+
+        if(Screen.hasShiftDown()) {
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.dowsing_rod.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.dowsing_rod.tooltip"));
+        }
+
+    }
+
+    private void outputValuableCoordinates(BlockPos blockPos, Player player, Block blockBelow) {
+        player.sendSystemMessage(Component.literal("Found " + blockBelow.getName() + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + "," + blockPos.getZ() + ")"));
+    }
+
+    private boolean isValuableBlock(BlockState state) {
+        return state.is(ModTags.Blocks.DOWSING_ROD_VALUABLES);
+    }
+}
